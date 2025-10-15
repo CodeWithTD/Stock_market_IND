c2902cc37e17f5b7fc773cb1a287ceaa1e1b5168
Tuhin <161478517+CodeWithTD@users.noreply.github.com>
2025-10-16 00:25:54 +0530

    Add files via upload

---
+++ Top gainers.sql
@@ -0,0 +1,11 @@
+INSERT INTO nse_data (symbol,percentage_change) VALUES
+	 ('CIPLA',7.23),
+	 ('DRREDDY',3.45),
+	 ('DIVISLAB',2.92),
+	 ('NESTLEIND',0.38),
+	 ('TCS',-0.19),
+	 ('BRITANNIA',-0.19),
+	 ('ASIANPAINT',-0.20),
+	 ('HINDUNILVR',-0.35),
+	 ('POWERGRID',-0.86),
+	 ('HCLTECH',-1.17);
